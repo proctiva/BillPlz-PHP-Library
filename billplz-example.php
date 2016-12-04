bc6fffc9939ff9d290858e991cc3c8cf62d6bc0f
proctiva <badri.sam@gmail.com>
2016-12-04 11:36:40 +0530

    Update billplz-example.php

--- billplz-example.php
+++ billplz-example.php
@@ -1,7 +1,7 @@
 <?PHP
 require_once ('billplz.php'); 
 
-$bill_id = billplz ($name, $email, $amount, $mobile, $callback, $delivery);
+$bill_id = billplz_create_bill ($name, $email, $amount, $mobile, $callback, $delivery);
 echo $bill_id; // Returns Bill ID
 
 ?>
